21d1ce4cdade111c8c01de9025cbb0b84ba958cd
Celestial-0 <proyash3053@gmail.com>
2025-03-17 21:09:32 +0530

    API Security

--- src/constants/allowed-origin.ts
+++ src/constants/allowed-origin.ts
@@ -1,8 +1,8 @@
 const ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    // 'https://yash-kumar-singh.vercel.app',
-    // 'https://yash-proyash3053-gmailcoms-projects.vercel.app',
-    // 'https://yash-git-dev-proyash3053-gmailcoms-projects.vercel.app',
+    'https://yash-kumar-singh.vercel.app',
+    'https://yash-proyash3053-gmailcoms-projects.vercel.app',
+    'https://yash-git-dev-proyash3053-gmailcoms-projects.vercel.app',
 ];
 
 export default ALLOWED_ORIGINS;
