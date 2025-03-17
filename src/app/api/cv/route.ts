8e4140e3a0e8715312271f0a1f085a51157b19a5
Celestial-0 <proyash3053@gmail.com>
2025-03-17 21:07:35 +0530

    API Security

--- src/app/api/cv/route.ts
+++ src/app/api/cv/route.ts
@@ -1,8 +1,9 @@
 // app/api/yash/route.ts
 import cv from '@/constants/cv.json';
+import { NextResponse } from 'next/server';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(_request: Request): Promise<NextResponse> {
+export async function GET(_request: Request): Promise<Response> {
   return new NextResponse(JSON.stringify(cv, null, 2), {
     status: 200,
     headers: { 'Content-Type': 'application/json' }
