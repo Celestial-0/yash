53ae1db498ae510b60a3c93671e6f0f567459faa
Celestial-0 <proyash3053@gmail.com>
2025-03-18 08:47:11 +0530

    origin update

--- src/constants/allowed-origin.ts
+++ src/constants/allowed-origin.ts
@@ -1,5 +1,6 @@
 const ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://yashkumarsingh.tech',
     'https://yash-kumar-singh.vercel.app',
     'https://yash-proyash3053-gmailcoms-projects.vercel.app',
     'https://yash-git-dev-proyash3053-gmailcoms-projects.vercel.app',
